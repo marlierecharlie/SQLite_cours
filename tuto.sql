c8b6a412595b8dbab3d3e92a56f9f82477dded80
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-07-20 13:41:32 +0200

    vidéo 6

---
+++ tuto.sql
@@ -0,0 +1,32 @@
+-- CREATE TABLE recipes (
+--     title VARCHAR(150),
+--     slug VARCHAR(50),
+--     content TEXT,
+--     duration SMALLINT,
+--     online BOOLEAN, 
+--     created_at DATETIME
+-- );
+
+
+--commande insert into
+-- rechercher le time stamp du jour
+-- INSERT INTO recipes( 
+--     title, 
+--     slug, 
+--     content, 
+--     duration, 
+--     online, 
+--     created_at
+-- ) VALUES (
+--     'Pastèque',
+--     'pastque',
+--     'contenu de test',
+--     1,
+--     TRUE, 
+--     1658316135
+--     );
+
+--requête SELECT
+
+
+
