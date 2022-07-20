7ae5244d6b891876052723fe55b6fc22231abb37
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-07-20 15:02:43 +0200

    début de la leçon

--- tuto.sql
+++ tuto.sql
@@ -27,6 +27,79 @@
 --     );
 
 --requête SELECT
+-- SELECT title, duration
+-- FROM recipes
+
+-- ou pour réccupérer toutes les infos 
+-- SELECT *
+-- FROM recipes
+
+--si on veut réccupérer sous certaines conditions seulement
+-- SELECT *
+-- FROM recipes
+-- WHERE online = FALSE
+
+-- SELECT *
+-- FROM recipes
+-- WHERE duration BETWEEN 0 AND 5
+
+-- SELECT *
+-- FROM recipes
+-- WHERE slug IN ('soupe', 'pastque')
+
+-- SELECT *
+-- FROM recipes
+-- WHERE slug LIKE 'pa%'
 
 
+-- opération booléen 
+-- TRUE AND TRUE = TRUE
+-- TRUE AND FALSE = FALSE 
+-- FALSE AND FALSE = FALSE 
+
+-- TRUE OR FALSE = TRUE 
+-- FALSE OR TRUE = TRUE 
+-- TRUE OR TRUE = TRUE 
+-- FALSE OR FALSE = FALSE
+
+-- SELECT *
+-- FROM recipes
+-- WHERE slug LIKE '%oupe'
+
+--commande insert into
+-- rechercher le time stamp du jour
+-- pour enregistrer plusieurs élèments les uns 
+-- derrière les autres 
+-- INSERT INTO recipes( 
+--     title, 
+--     slug, 
+--     content, 
+--     duration, 
+--     online, 
+--     created_at
+-- ) VALUES (
+--     'Pastèque2',
+--     'pastque',
+--     'contenu de test',
+--     1,
+--     TRUE, 
+--     1658316135
+--     ),
+--     (
+--     'Pastèque1',
+--     'pastque',
+--     'contenu de test',
+--     1,
+--     TRUE, 
+--     1658316135
+--     )
+
+--DELETE
+DELETE FROM recipes WHERE title = 'Pastèque3'
+
+--UPDATE
+UPDATE recipes SET title = 'Soupe maison' WHERE title = 'Soupe'
+UPDATE recipes SET slug = 'pasteque' WHERE slug = 'pastque'
+
+SELECT* FROM recipes 
 
